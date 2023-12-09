5859f5807ec8fbb40c66292ee7313c3c712fece3
Tobiasz Libich <tobiasz230@wp.pl>
2023-12-09 18:00:40 +0100

    reward points counter (wip)

--- src/_shared/utils/_rewardPointCounter.js
+++ src/_shared/utils/_rewardPointCounter.js
@@ -1,15 +1,4 @@
-const fakeBasePointValue = 1;
-
-const fakeRewardRanges = [
-    {from: 50, to: 100, multiple: 1},
-    {from: 100, to: null, multiple: 3}
-]
-
-const fakePrice = 122.888;
-
-
-// TODO: remove fake data;
-export const getRewardPointsByTransaction = (price = fakePrice, basePointValue = fakeBasePointValue,  pointRanges = fakeRewardRanges) => {
+export const getRewardPointsByTransaction = (price, basePointValue,  pointRanges) => {
     const priceWithoutRest = Math.floor(price);
 
     return pointRanges.reduce((points, pointRange) => {
@@ -19,9 +8,7 @@ export const getRewardPointsByTransaction = (price = fakePrice, basePointValue =
         switch(true) {
             case (priceWithoutRest > from && (!to || priceWithoutRest <= to)):
                 return points += (priceWithoutRest - from) * multiplier 
-            case(price > from && priceWithoutRest <= to):
-                return points += (to - from) * multiplier 
-            case(price > from && priceWithoutRest > to):
+            case(priceWithoutRest > from && priceWithoutRest >= to):
                 return points += (to - from) * multiplier 
             default:
                 return 0;
