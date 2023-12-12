266028c80e1941afe8cb7f5783fddf84cdc963aa
Tobiasz Libich <tobiasz230@wp.pl>
2023-12-13 00:45:04 +0100

    memoized transactions

--- src/app/containers/transactions/CustomerTransactions.jsx
+++ src/app/containers/transactions/CustomerTransactions.jsx
@@ -1,28 +1,30 @@
 import useCustomerTransations from "../../hooks/transaction/useCustomerTransactions";
 import Section from "../../components/section/Section";
 import TransactionsTable from "../../components/transactionsTable/TransactionsTable";
+import { useMemo } from "react";
 
 const CustomerTransactions = ({customerId}) => {
-    const {transactions} = useCustomerTransations(customerId)
+    const {transactions} = useCustomerTransations(customerId);
 
-    if(!transactions.length) return <span>The customer has not made any transaction</span>
-
-    const transactionsByMonth = transactions.reduce((acc, transaction) => {
+    const transactionsByMonth = useMemo(() => transactions
+    .reduce((acc, transaction) => {
         const date = new Date(transaction.createdDate);
         const key = date.toLocaleString(document.documentElement.lang, {month:'long', year:'numeric'});
         const formattedDate = date.toLocaleString(document.documentElement.lang);
 
         if (!acc[key]) acc[key] = {transactions: [], totalSum: 0, currency: transaction.currency, totalPoints: 0}
-        
 
         acc[key].transactions = [...acc[key].transactions, {...transaction, createdDate: formattedDate}];
         acc[key].totalPoints += transaction.rewardPoints;
         acc[key].totalSum += transaction.price;
 
         return acc;
-    }, {});
+    }, {}), [transactions]);
 
-    const allPoints = Object.keys(transactionsByMonth).reduce((acc, key) => acc += transactionsByMonth[key].totalPoints, 0);
+
+    const allPoints = useMemo(() => Object.keys(transactionsByMonth).reduce((acc, key) => acc += transactionsByMonth[key].totalPoints, 0), [transactionsByMonth]);
+
+    if(!transactions.length) return <span>The customer has not made any transaction</span>
 
     return (
         <Section subheader={'Customer points'} subheaderEnd={<span>all points: <strong>{allPoints}</strong></span>}>
