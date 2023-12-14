68f0086b3821014df169e3844e1d39da13f3d9fe
Tobiasz Libich <tobiasz230@wp.pl>
2023-12-14 12:20:38 +0100

    fix imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import CustomersList from "./app/features/customerTransactions/containers/CustomersList";
 import CustomerTransactions from "./app/features/customerTransactions/containers/CustomerTransactions";
-import Box from "./_shared/ui/box/Box";
-import Container from "./_shared/ui/container/Container";
+import Box from "./_shared/components/box/Box";
+import Container from "./_shared/components/container/Container";
 import { CustomerTransactionsProvider } from "./app/features/customerTransactions/context/CustomerTransactionsContext";
 import "./App.css";
 
